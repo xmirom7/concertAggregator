fab17c0cd55df04ab15fcef2d3dd10355acf9ca0
Miroslav Nagy <miroslav.nagy@centrum.sk>
2019-03-29 17:38:30 +0100

    Added bands. Tables bands and modules are emptied before addition.

--- dataInit.sql
+++ dataInit.sql
@@ -1,5 +1,8 @@
 PRAGMA foreign_keys = on;
 
+DELETE FROM bands;
+DELETE FROM modules;
+
 INSERT INTO modules
 VALUES
 (0, 'Bandsintown'),
@@ -9,4 +12,9 @@ INSERT INTO bands (name, module_id, module_arg)
 VALUES
 ('Konflikt', 0, 'Konflikt,official'),
 ('Rammstein', 0, 'Rammstein'),
-('IDLES', 1,'1352869-idles');
+('IDLES', 1,'1352869-idles'),
+('SLAVES', 1, '22255-slaves'),
+('Sum 41', 0, 'sum 41'),
+('Dropkick Murphys', 0, 'dropkick murphys'),
+('Polemic', 0, 'polemic'),
+('Bad Manners', 0, 'bad manners');
