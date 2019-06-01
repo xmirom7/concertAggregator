aa27ab340e34a106755d7f143c3bd861669a2597
Miroslav Nagy <miroslav.nagy@centrum.sk>
2019-06-02 00:10:44 +0200

    Added bands.

--- dataInit.sql
+++ dataInit.sql
@@ -10,10 +10,13 @@ VALUES
 
 INSERT INTO bands (name, module_id, module_arg)
 VALUES
-('Konflikt', 0, 'Konflikt,official'),
-('Rammstein', 0, 'Rammstein'),
+('Madness', 1, '1959190-madness'),
+('Sleaford Mods', 1, '635776-sleaford-mods'),
+('Shame', 1, '134405-shame'),
 ('IDLES', 1,'1352869-idles'),
 ('SLAVES', 1, '22255-slaves'),
+('Konflikt', 0, 'Konflikt,official'),
+('Rammstein', 0, 'Rammstein'),
 ('Sum 41', 0, 'sum 41'),
 ('Dropkick Murphys', 0, 'dropkick murphys'),
 ('Polemic', 0, 'polemic'),
